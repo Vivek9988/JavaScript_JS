ed40e7c46b638dea98cbbaabcd987f438aa28e3e
Vivek9988 <vivek19102004@gmail.com>
2024-06-12 22:41:20 +0530

    first commit

---
+++ 02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict " // treat all JS code as newer version.
+
+// aleart (3+3) // we are using nodejs, not browser
+
+let name = "hitesh ";
+let age = 18;
+let isLooggendIn= false;
+
+
+// 1)-number =>2 to power 53;
+// 2)-bigint
+// 3)-string => ""
+// 4)-boolean => true , false
+// 5)-null  =>standlone value , means empty
+// 6)-undefined => meaning is that its value is not defined , nut initilized
+// 7)-symbol=>unique
+
+// object 
+
+console.log(typeof undefined);  //undefined
+console.log(typeof null); //object
---
+++ 03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score ="vivek"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valurInNumber = Number(score);
+console.log(typeof valurInNumber);
+console.log(valurInNumber);
+
+// "33"  => 33
+// "33abc" =>NaN
+// true => 1; false = 0 
+
+
+let isLoggendIn="hitesh";
+let booleanIsLoggedIn=Boolean(isLoggendIn); 
+
+console.log(booleanIsLoggedIn);
+
+// 1  => true 0 =>false
+// "" =>false
+// "hitesh" =>ture;
+ 
+
+let someNumber = 33;
+
+
+let stringNumber= String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
