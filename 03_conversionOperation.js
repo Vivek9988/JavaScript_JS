9163083d27e19459fc75e76d788524ee23e5bd30
Vivek9988 <vivek19102004@gmail.com>
2024-06-14 22:15:19 +0530

    day 06

--- 03_conversionOperation.js
+++ 03_conversionOperation.js
@@ -1,31 +1,70 @@
-let score ="vivek"
+// let score ="33"
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
-let valurInNumber = Number(score);
-console.log(typeof valurInNumber);
-console.log(valurInNumber);
+// let valurInNumber = Number(score);
+// console.log(typeof valurInNumber);
+// console.log(valurInNumber);
 
 // "33"  => 33
 // "33abc" =>NaN
 // true => 1; false = 0 
 
 
-let isLoggendIn="hitesh";
-let booleanIsLoggedIn=Boolean(isLoggendIn); 
+// let isLoggendIn="hitesh";
+// let booleanIsLoggedIn=Boolean(isLoggendIn); 
 
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1  => true 0 =>false
 // "" =>false
 // "hitesh" =>ture;
  
 
-let someNumber = 33;
+// let someNumber = 33;
 
 
-let stringNumber= String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let stringNumber= String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+
+
+//******************************************** Operation ************************************************//
+
+
+let value = 3 ;
+let negValue=-value;
+console.log(negValue);
+
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**2);
+//console.log(2*2);
+//console.log(2/2);
+//console.log(2%2);
+
+let str1 = "vivek";
+let str2 = "raj";
+
+let str3 = str1+str2;
+
+console.log(str3);
+
+console.log(1 + "2");
+console.log("1"+2);
+console.log("1"+ 2+ 2);
+console.log(1+2+"3");
+
+
+
+
+
+
+
+
 
---
+++ datatype-summary.js
@@ -0,0 +1,68 @@
+// primitive
+
+// 7 types : String, Number, Boolean , null, undefined, Symbol, BigInt
+
+
+// const score = 100;
+// const scoreValue= 100.3;
+// const isLoggendIn = false;
+// const outsideTemp=null;
+// let userEmail;
+
+
+//Reference (Non primitive)
+
+// Arrays, Objects, Function
+
+
+// const heros = [ " shaktiman " , "balveer" , "kaliya"];
+// let myobj={
+//     name:"vivek",
+//     age:22,
+
+// 
+// const myFunction = function(){
+//     console.log("hello")
+// }
+
+
+//***************************************** */
+
+
+// Stack (Primitive)  , Heap (Non - premitive)
+
+
+// let vivek = 35;
+
+// let vidhi = 35;
+// vidhi=34
+
+// console.log(vivek);
+// console.log(vidhi);
+
+
+// let user ={
+//     age:34,
+//     name:"vivek"
+
+// }
+
+// let user2=user;
+
+// console.log(user);
+// console.log(user2);
+
+// user2.age=45;
+// console.log(user2);
+// console.log(user);
+
+
+
+
+
+
+
+
+
+
+
