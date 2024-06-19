3fad03ea626b4ff82606287d0903f7e0851ed77d
Vivek9988 <vivek19102004@gmail.com>
2024-06-19 16:27:23 +0530

    changes

--- 03_basic/02_scope.js
+++ 03_basic/02_scope.js
@@ -15,7 +15,7 @@ if (true) {
 
 
 function one() {
-    const username = "hitesh"
+    const username = "vivek"
 
     function two() {
         const website = "youtube"
@@ -30,8 +30,8 @@ function one() {
  one()
 
 if (true) {
-    const username = "hitesh"
-    if (username === "hitesh") {
+    const username = "vivek"
+    if (username === "vivek") {
         const website = " youtube"
         // console.log(username + website);
     }
