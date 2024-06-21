c5271a9aca630f403684f49a5adefe1fb69086f2
Vivek9988 <vivek19102004@gmail.com>
2024-06-21 19:40:14 +0530

    iterations

---
+++ 05_Iterations/seven.js
@@ -0,0 +1,37 @@
+const myNumber=[1,2,3,4,5,6,7,8,9,10];
+
+//console.log(myNumber)
+
+
+// method 1 to do
+const number1 = myNumber.map((num)=>{
+     num+10;
+    // console.log(num)
+ 
+});
+//console.log(number1);
+
+
+// method 2 to do
+const number= myNUmber.filter((num)=> { 
+     //console.log(num);
+        //return num;
+    })
+    //console.log(number);
+
+    // method 3 to do
+    const number2=myNUmber.map((num)=>{num+10});
+    console.log(number2)
+     
+
+
+//****************************  chaining  ***********************/
+
+const newNums = myNumers
+    .map((num) => num * 10)
+    .map((num) => num + 1)
+    .filter((num) => num >= 40)
+
+console.log(newNums);
+
+ 
