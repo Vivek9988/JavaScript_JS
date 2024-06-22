b4ab2d6740fbed17ef68914651ff4336ab917ce1
Vivek9988 <vivek19102004@gmail.com>
2024-06-22 10:33:34 +0530

    dom

---
+++ 06_dom/one.js
@@ -0,0 +1,10 @@
+let heading =document.getElementById("heading");
+console.dir(heading);
+let h1=heading.tagName;
+let h2=heading.innerText;
+console.log(h2)
+console.log(h1)
+
+
+let vivek=document.getElementsByClassName("vivek");
+//console.log(vivek)
