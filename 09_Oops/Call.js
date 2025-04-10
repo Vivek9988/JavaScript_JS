e9c8b43f7ddcb6db721bd1144ec02c49f9b2ee5a
Vivek9988 <vivek19102004@gmail.com>
2025-04-10 10:02:39 +0530

    new code

--- 09_Oops/Call.js
+++ 09_Oops/Call.js
@@ -1,16 +1,1 @@
-function setUsername(username){
-    this.username=username;
-
-}
-
-function createUser(username,email,password){
-    setUsername.call(this,username)
-
-
-    this.email=email;
-    this.password=password;
-}
-
-const vivek = new createUser("hello","vivek@124","123")
-
-console.log(vivek);
+w
